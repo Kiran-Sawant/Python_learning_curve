a770598eff97466ede33620aa46bd47650bbad29
Kiran-Sawant <kbsawant1005@gmail.com>
2020-05-05 00:44:14 +0530

    numerical operations

---
+++ Basics/Decimals & Fractions.py
@@ -0,0 +1,23 @@
+#___________________________decimals____________________________#
+import decimal as dc
+
+dc.getcontext().prec = 4  # setting the decimal precision globally
+k = dc.Decimal('1')       # deimal literal
+f = dc.Decimal('7')
+print(k)
+print(f)
+with dc.localcontext() as ctx: # creating a local context manager as ctx
+    ctx.prec = 2               # settin local context precision of 2 decimal places
+    print(k/f)                 # answer with local context pricision
+
+print(k/f) # answer with global context pricision
+
+print('='*40)#_____________________Fractions________________________#
+import fractions as fr
+
+x = fr.Fraction(1, 7) # Fractional number literal
+y = fr.Fraction(0.25) # converting a float to fraction
+z = fr.Fraction(k/f)  # converting a decimal to fraction
+print(x)
+print(y)
+print(z) # numbers are automatically simplified to GCD ie. 4/6 ~ 2/3
---
+++ Basics/Division.py
@@ -0,0 +1,18 @@
+import math
+
+k = 14
+j = -4
+i = k/j  # division passing float
+g = k//j # division passing floored answer
+m = k%j  # modulo passes the remainder of truncated k/j
+
+print("k = ", k)
+print("j = ", j)
+print("k/j = ", i)
+print("k//j = ", g)
+print('k % j = ', m)
+
+print("="*40)
+print('k/j truncated: ', math.trunc(k/j))
+print('k/j floored: ', math.floor(k/j))
+print('k/j rounded: ', round(k/j))
