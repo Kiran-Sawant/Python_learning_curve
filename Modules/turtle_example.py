363d1a6417f432c5adc78febad378a0e681af1b4
Kiran-Sawant <kbsawant1005@gmail.com>
2020-05-05 11:11:15 +0530

    working with turtle module

---
+++ Modules/turtle_example.py
@@ -0,0 +1,11 @@
+from turtle import * #imports everything without an underscore
+                     #with this import you do not need to type the name of module before its methods
+                     #however, the IDE does not recognize its methods either
+# import time
+ 
+forward(150)
+right(250)
+forward(142)
+circle(100)
+done()
+# time.sleep(5) #keeps the window open for 5 seconds
