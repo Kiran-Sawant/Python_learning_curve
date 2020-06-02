3bed38befd2c014741ade5c0f4bd45b21f5e230b
Kiran-Sawant <kbsawant1005@gmail.com>
2020-06-02 13:56:59 +0530

    Python json module

---
+++ Modules/Python Standard Library/json/json syntaxes.py
@@ -0,0 +1,41 @@
+import json
+
+# true JSON data (don't use '' only "")
+people_string = """
+{
+    "people": 
+    [
+        {
+            "name": "John Smith",
+            "phone": "9323-495-xx2",
+            "emails": ["kbsawant@gmail.com", "kenrosenberg@gmail.com"],
+            "has_license": false
+        },
+        {
+            "name": "Jane Doe",
+            "phone": "564-789-1234",
+            "emails": null,
+            "has_license": true
+        }
+    ]
+}
+"""
+
+# loading JSON string into a python variable
+data = json.loads(people_string)
+for i in data['people']:
+    del i['phone']
+
+# loading a python object injo a JSON string
+new_string = json.dumps(data, indent=2)
+
+#_______Working with JSON files_________#
+
+with open("states.json") as jay:
+    data = json.load(jay)               # .load is used with file objects
+
+    for state in data['states']:
+        del state['area_codes']
+
+with open('new_states.json', mode='w') as jay2:
+    json.dump(data, jay2, indent=2)     # .dump is used to write to a JSON file
