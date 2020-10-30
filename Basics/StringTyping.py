6aebd624ad73ef74978fc824a81ec0edb5452db0
Kiran-Sawant <kbsawant1005@gmail.com>
2020-10-30 19:54:33 +0530

    Changed Comment on line 7

--- Basics/StringTyping.py
+++ Basics/StringTyping.py
@@ -4,5 +4,5 @@ print(string,'\n' ,string2)
 
 #Sequence Operations
 string3 = "Killer Bean Forever"
-print(string3[:6],'\n', string3[7:11],'\n', string3[12:19]) #[:n] selects string objects fron start to n-1
+print(string3[:6],'\n', string3[7:11],'\n', string3[12:19]) #[:n] selects string objects from 0 to n-1, ie 0 to 5.
                                                             #[7:11] selects string objects from 7 to 10
