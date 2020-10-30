6fb003ed2ba628d5271985f701048c15b65767df
Kiran-Sawant <kbsawant1005@gmail.com>
2020-10-30 20:05:01 +0530

    Changed Comments.

--- Basics/join() & split() method.py
+++ Basics/join() & split() method.py
@@ -1,5 +1,5 @@
 myList = ["a", "b", "c", "d"]
-newString = ", ".join(myList) # join() method creates a string with the values passed in it, delimited by the thing passed in the string
+newString = ", ".join(myList) # join() method creates a string with the values passed in it, delimited by the applied string.
 print(newString) 
 
 splitString = newString.split(", ") #split is the opposit of join, creates a list of all objects with a delimiter between them
