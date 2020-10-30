314e04cebc8c0a09717a8a1b721ef3b89e1a4c93
Kiran-Sawant <kbsawant1005@gmail.com>
2020-10-30 23:05:50 +0530

    python whileloop

--- Basics/Control statements/whileLoop.py
+++ Basics/Control statements/whileLoop.py
@@ -1,13 +1,13 @@
-i = 1 #While loop needs initialization of the variable
-while i < 10: #While loop performs iteration on the basis of logical condition applied on the pre defined variable
+i = 1         # While loop needs initialization of the variable
+while i < 10: # While loop performs iteration on the basis of logical condition applied on the pre defined variable
     print("i is now {0}".format(i))
-    i += 1 #Augmented assignment
+    i += 1    # Augmented assignment
 
 print("="*20 + "Direction Game\n") #_________Direction Game____________#
 
 exits = ["east", "north east", "south"]
 
-chosenExit = "" #Variable Initialization
+chosenExit = "" # Variable Initialization
 while chosenExit not in exits:
     chosenExit = input("please choose any direction: ")
     if chosenExit == "quit":
