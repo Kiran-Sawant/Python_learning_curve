cadd28ba1eb527df3cef002d6f16db46ca49ad09
Kiran-Sawant <kbsawant1005@gmail.com>
2020-10-31 14:11:23 +0530

    writing to a text file & python eval() function

--- Basics/Python file handling/Python file writing.py
+++ Basics/Python file handling/Python file writing.py
@@ -10,7 +10,7 @@ cities = []
 
 with open("cities.txt", mode='r') as cityFile:
     for city in cityFile:
-        cities.append(city.strip('\n')) #strip() function moves the passed string fron the start & end of the retrived string
+        cities.append(city.strip('\n')) # strip() method removes the passed string from the start & end of the retrived string.
 for i in cities:
     print(i)
 
@@ -19,15 +19,18 @@ print("="*50)#________________eval() function_________________________#
 imelda = "More Mayhem", "Imelda May", "2011", ((1, "Pulling the rug"),
          (2, "Psyco"), (3, "Mayhem"), (4, "Kentish town walt"))
 
-with open("imelda.txt", mode='w') as imeldaFile: #creating the file
-    print(imelda, file=imeldaFile) # writing to file
+with open("imelda.txt", mode='w') as imeldaFile: # creating the file
+    print(imelda, file=imeldaFile)               # writing to file
 
-#______________reading our file________________#
+# reading our file
 with open("imelda.txt", mode='r') as imeldaFile2:
     contents = imeldaFile2.readline()
 
-imelda = eval(contents) #eval() function evaluates the text line to determine which built-in object type it is & assigns it to the variable
-Album, Artist, year, tracks = imelda #unpacking the tuple
+"""eval() function evaluates the text line to determine which python built-in object type it is & assigns it to the variable.
+In this case the text resembles a tuple, therefore imelda will be a tuple."""
+
+imelda = eval(contents)
+Album, Artist, year, tracks = imelda # unpacking the tuple
 print(Album)
 print(Artist)
 print(year)
