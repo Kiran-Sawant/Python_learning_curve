8d3bc48c80ac5c391c70b8e26f6dc0ebe39d07d7
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-01 01:34:40 +0530

    learning inheritance & method overriding

--- OOP/Game/enemy.py
+++ OOP/Game/enemy.py
@@ -1,4 +1,4 @@
-"""Inheritance, Method overriding"""
+"""Inheritance(line 30), Method overriding(line 54)"""
 
 import random
 
@@ -6,7 +6,7 @@ class Enemy:
 
     def __init__(self, name='Enemy', hit_points=0, lives=1):
         self.name = name
-        self.hit_points= hit_points
+        self.hit_points = hit_points
         self.lives = lives
         self.alive = True
 
@@ -31,7 +31,7 @@ class Troll(Enemy):         #The class Troll inherits from its superclass Enemy
     
     def __init__(self, name):
         # super(Troll, self).__init__(name=name,  hit_points=23, lives= 1)      #inheriting __init__ of super class
-        super().__init__(name=name, hit_points=26, lives=1)                     #same as line 29
+        super().__init__(name=name, hit_points=26, lives=1)                     #same as line 33
 
     def grunt(self):        #A property of troll class or an attribute of an instance of troll class
         print('Me {0.name}, {0.name} Stomp you!!!'.format(self))
@@ -56,12 +56,16 @@ class Vampyre(Enemy):       #The class Vampyre inherits from class Enemy
             super().take_damage(damage=damage)      #calling a method of the super class
 
 #_____Making a supersubclass_____#
-class VampyreKing(Vampyre): #The class Vampyre inherits from class Enemy
-                            #the class VampyreKing is a supersub class of Enemy class
+class VampyreKing(Vampyre):
+    """The class Vampyre inherits from class Enemy,
+    the class VampyreKing is a supersub class of Enemy class.
+    VampyreKing class overrides the method mentioned in its super-class
+    which overrides the method in Enemy class."""
 
     def __init__(self, name):
         super().__init__(name)
         self.hit_points = 140
 
+    # Method overriding
     def take_damage(self, damage):
         super().take_damage(damage // 4)
