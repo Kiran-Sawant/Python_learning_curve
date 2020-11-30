c9066c884989608cb85601b5ed6e34e05f048594
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-01 00:31:09 +0530

    Python filter() function

--- Basics/In-built functions/filter_test.py
+++ Basics/In-built functions/filter_test.py
@@ -1,18 +1,24 @@
+"""filter(f, i) takes a function name & an iterable as arguments.
+returns the iterable values for which the function returns True.
+Unlike map() function filter() does not return the outcome of the
+function but the value in the passed iterable for which the function
+returns True."""
+
+# Creating an iterable
 menu = [["egg", "spam", "bacon"],
         ["egg", "sausage", "bacon"],
         ["egg", "spam"],
         ["egg", "bacon", "spam"],
         ['Cheeze', 'Jam', 'Tost']]
 
+# Filtering without filter()
 for meal in menu:
     if 'spam' not in meal:
         print(meal)
 
 print('-'*40)
 
-"""filter(f, i) takes a function name & an iterable as arguments.
-returns the iterable values for which the function returns True."""
-
+#________filter() function__________#
 # returns False if spam in meal
 def not_spam(meal: list):
     return 'spam' not in meal
