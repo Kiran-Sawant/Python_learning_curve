24eb83f19c64f2b04f10e8b1c322633a67e61109
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-01 00:38:16 +0530

    locals() & globals()

--- Basics/In-built functions/locals() & globals().py
+++ Basics/In-built functions/locals() & globals().py
@@ -1,17 +1,20 @@
+"""The locals() & globals() functions give a dictionary of local and global variables
+of the namespace in which they are executed"""
+
 class Kettle(object):
 
-    #Creating class attributes
+    # Creating class attributes
     power_source = 'Electricity'
     print(f"Dict of global variables to Kettle: {globals()}\n")
     print(f"Dict of local variables to Kettle: {locals()}\n")
-    #creating a constructor method
+    # Creating a constructor method
     def __init__(self, make, price):
         self.make = make
         self.price = price
         self.switch = False
         print(f"Dict of global variables to Kettle.__init__: {globals()}\n")
         print(f"Dict of local variables to Kettle.__init__: {locals()}\n")
-    #Creating a method
+    # Creating a method
     def switch_on(self):
         self.switch = True
 
