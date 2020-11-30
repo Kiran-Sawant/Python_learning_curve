018e06ed4799276136e00607db6d3add0186aa88
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-01 00:24:57 +0530

    chr() & ord() function

--- Basics/In-built functions/chr() & ord().py
+++ Basics/In-built functions/chr() & ord().py
@@ -4,11 +4,11 @@
     https://en.wikipedia.org/wiki/List_of_Unicode_characters"""
 
 k = chr(198)
-print(k)
+print(f'chr(198): {k}')
 
 #_____________ord()______________#
-"""The ord() function returns the number representing the unicode code
-    of a specified character."""
+"""The ord() function returns the unicode of charecters
+    in the passed string."""
 
 j = ord('&')
-print(j)
+print(f"ord('&'): {j}")
