69fad7a81510a78974ebba35f83f6c242afa4dc1
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-04 15:29:25 +0530

    added documentation

--- Used Defined Functions/decorators.py
+++ Used Defined Functions/decorators.py
@@ -1,3 +1,22 @@
+"""A decorator function consists of a First-class function that takes
+the function on which it is decorated on, as an argument, and returns the
+closure function.
+    The closure function can take arguments that it will pass to the
+decorated function. It performs the defined operation and finally returns
+the original decorated function.
+    The closure can access variables in its non-local scope even after the
+first-class function is done executing.
+    Decorator functions are used to perform tasks that might needed to be
+performed everytime before a function is called, eg: logging the time & arguments
+of a function call, making a database entry everytime a function is called, etc.
+
+    Decorator classes are implimented using a __call__() Magic method. This 
+method makes the object callable, ie. whenever the object is called using a
+pair of parenthesis (object_name()) the suite inside the __call__() method
+will be executed.
+    __init__() acts as an outer function and the __call__() function perform
+the operation and returns the decorated function."""
+
 #____________Defining a decorator Function____________#
 def decorator_func(original_func):
 
