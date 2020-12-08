2ca1d10bcbe6f0e0882ee5f74a0821fdf02fa1dc
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-08 12:38:45 +0530

    added documentation

--- Metatrader5 python/Profit & Margin calc.py
+++ Metatrader5 python/Profit & Margin calc.py
@@ -15,7 +15,7 @@ print('\nMargin requirement for buying {0} @ {1} is ${2}'.format(order[0].symbol
 
 
 #____________Calculating Profit/loss_____________#
-volatility = float(input('Enter the desired volatility in percentage: '))
-percentage = volatility * (order[0].price_open / 100)
+volatility = float(input('Enter the desired volatility in percentage: '))       # % increase in price
+percentage = volatility * (order[0].price_open / 100)                           # gain in asset price
 profit = mt5.order_calc_profit(mt5.ORDER_TYPE_BUY, order[0].symbol, order[0].volume_initial, order[0].price_open, (order[0].price_open + percentage)) #returns potential P/L in account currency
 print('\n Profit on {0} at {1} with {2}% volatility is ${3}'.format(order[0].symbol, order[0].price_open, volatility, profit))
