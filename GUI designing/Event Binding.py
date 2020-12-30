ac44826fddaacc8dbd511126b15d3df4ecc802de
Kiran-Sawant <kbsawant1005@gmail.com>
2020-12-30 14:20:25 +0530

    Garbage code

---
+++ GUI designing/Event Binding.py
@@ -0,0 +1,17 @@
+"""Read this documentation:-
+https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/events.html"""
+
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('420x360')
+
+# var = tk.StringVar()
+# var.set("KungFu Panda")
+
+label = tk.Label(root, text='KungFu Panda')
+label.pack()
+
+root.mainloop()
+
+# print(var.get())
