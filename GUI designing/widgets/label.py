7a98db1d51bd77825f97cd0a45063533bf24a87d
Kiran-Sawant <kbsawant1005@gmail.com>
2021-01-01 15:52:16 +0530

    Making Mutable Labels

--- GUI designing/widgets/label.py
+++ GUI designing/widgets/label.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 
+def change():
+
+    if var.get() == 'Killer Bean':
+        var.set('KungFu Panda')
+    else:
+        var.set('Killer Bean')
+
 # Initiating window
 mainWindow = tk.Tk()
 mainWindow.geometry('440x440')
@@ -8,6 +15,18 @@ mainWindow.title('Label Example')
 # creating a label
 l1 = tk.Label(master=mainWindow, text="Hello ugly world!")
 # placing a label
-l1.pack()
+l1.grid(row=0, column=0)
+
+#_______Making a Mutable Label__________#
+"""Link a StringVar to a label to and use the .set()
+method on the String Var to make the label, mutable"""
+var = tk.StringVar()
+var.set('Killer Bean')
+
+l2 = tk.Label(master=mainWindow, textvariable=var)
+l2.grid(row=1, column=0)
+
+buton = tk.Button(master=mainWindow, text='change', command=change)
+buton.grid(row=2, column=0)
 
 mainWindow.mainloop()
