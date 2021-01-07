a2ebfb7af625cbcf8368dacb093a0ca0a41f83d2
Kiran-Sawant <kbsawant1005@gmail.com>
2021-01-07 23:55:26 +0530

    executing external scripts  from python

---
+++ Modules/Python_Standard_Library/subprocess/subprocess example.py
@@ -0,0 +1,16 @@
+"""subprocess module can be used to invoke external programs(subprocesses) through OS shell.
+This script invokes another python script through shell, using the .run() method.
+One can invoke scripts written in other languages as well in the same way."""
+
+import subprocess as sub
+
+# setting the path of target script.
+path = __file__.replace('subprocess example.py', 'sys_module.py')
+# setting the script to run.
+script = "python " + path
+
+# print(path)
+# print(script)
+
+# running the script in OS default shell.
+sub.run(script, shell=True)
