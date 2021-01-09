b10d694c4fea2f13f0758fb79c2e8f4fc739458e
Kiran-Sawant <kbsawant1005@gmail.com>
2021-01-09 16:35:44 +0530

    added sys attributes and methods

--- Modules/Python_Standard_Library/sys_module.py
+++ Modules/Python_Standard_Library/sys_module.py
@@ -1,10 +1,15 @@
-"""The sys module can be used to access this script externally through the shell.
+""" The module provides access to some variables used by the interpreter to
+function and interact with the interpreter.
+    The sys module can be used to access this script externally through the shell.
 both sys & subprocess modules can be used to communicate between two different
 languages through the shell.
     sys is used to make a script that will recieve commands from the shell.
-    subprocess is used to make a script that will send commands to the shell."""
+    subprocess is used to make a script that will send commands to the shell.
+    This script shows some useful variables.
+PyDoc: https://docs.python.org/3.7/library/sys.html"""
 
 import sys
+import pprint
 
 sys.stderr.write("This is how stderr text looks\n")
 sys.stderr.flush()
@@ -19,3 +24,31 @@ here, sys_module.py, "Hello" & "Its me" are arguments."""
 # print(f"The first argument is: {sys.argv[0]}")
 for i in sys.argv:
     print(i)
+
+#____names of all modules compiled in python interpreter_____#
+print(sys.builtin_module_names)
+
+#____Gives the absolute path of python interpreter______#
+print(sys.executable)
+
+#____size of the object in bytes______#
+print(sys.getsizeof("Get_off_my_lawn"))
+
+#___names of all the loaded modules___#
+pprint.pprint(sys.modules)
+
+#__list of paths where interpreter searches for modules__#
+print(sys.path)
+
+#____string containing info of platform (OS)______#
+print(sys.platform)
+# check if the OS is windows
+print(sys.platform.startswith('win32'))
+# check if the OS is linux
+print(sys.platform.startswith('linux'))
+
+#___returns python interpreter version_____#
+print(sys.version)
+
+#____returns C API version_____#
+print(sys.api_version)
