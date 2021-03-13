aa4e25205119f940ee7f249b01e4c5eeaf28b22f
Kiran-Sawant <kbsawant1005@gmail.com>
2021-03-13 15:21:05 +0530

    added comment

--- Modules/Python_Standard_Library/threading/threading Timer.py
+++ Modules/Python_Standard_Library/threading/threading Timer.py
@@ -1,10 +1,12 @@
-"""This class represents an action that should be run only
-after a certain amount of time has passed — a timer."""
+"""This class represents an action that should run only
+after a certain amount of time has passed — a timer.
+Every Timer instance runs on its own thread."""
 
 from threading import Timer
 
 def Hello(name):
     print(f'Hello {name}')
 
+# Timer Object with an interval of 5 sec
 t = Timer(5, Hello, ('Kiran',))
 t.start()
