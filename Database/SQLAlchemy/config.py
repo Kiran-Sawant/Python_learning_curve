e14e57c49d1b81d63614111f53e989fbc8c8d0f2
Kiran-Sawant <kbsawant1005@gmail.com>
2021-05-01 14:19:31 +0530

    database configuration

---
+++ Database/SQLAlchemy/config.py
@@ -0,0 +1,3 @@
+# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+DATABASE_URI = r"postgresql+psycopg2://postgres:MargretThacheris100%sexy@localhost:5432/books"
