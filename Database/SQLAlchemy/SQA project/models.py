6371ea4871c92fbc49bca179f254ae935f6377e8
Kiran-Sawant <kbsawant1005@gmail.com>
2021-05-28 17:55:59 +0530

    ORM model of the project

--- Database/SQLAlchemy/SQA project/models.py
+++ Database/SQLAlchemy/SQA project/models.py
@@ -2,6 +2,7 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (Column, Integer, String, Date)
+from sqlalchemy.dialects.postgresql import MONEY
 
 # Creating an instance of declarative base
 # Our models need to inherit this instance, for SQA to recognise our models.
@@ -15,6 +16,7 @@ class Book(Base):
     author      = Column(String)
     pages       = Column(Integer)
     published   = Column(Date)
+    price       = Column(MONEY)
 
     def __repr__(self):
         return f"{self.title} by {self.author}"
