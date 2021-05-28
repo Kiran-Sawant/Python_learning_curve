118d643d0b3484c559e1785c2b78727a12b0b98d
Kiran-Sawant <kbsawant1005@gmail.com>
2021-05-28 17:58:57 +0530

    script to add price

---
+++ Database/SQLAlchemy/SQA project/cli.py
@@ -0,0 +1,17 @@
+from crud import engine
+from models import Book
+from sqlalchemy.orm.session import sessionmaker
+
+Session = sessionmaker(bind=engine)
+
+s = Session()
+
+books = s.query(Book).all()
+
+for book in books:
+    price = int(input(f"Insert price for {book.title}: "))
+    book.price = price
+    s.add(book)
+
+s.commit()
+s.close()
