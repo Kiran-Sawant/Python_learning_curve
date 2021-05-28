88bb4bdbaf270e9d9dd25ad41734a3e79ac37a50
Kiran-Sawant <kbsawant1005@gmail.com>
2021-05-28 17:57:45 +0530

    database config file

---
+++ Database/SQLAlchemy/SQA project/config.py
@@ -0,0 +1,3 @@
+# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+DATABASE_URI = r"postgresql+psycopg2://postgres:your-postgres-user-password@localhost:5432/database_name"
--- Database/SQLAlchemy/config.py
+++
@@ -1,3 +0,0 @@
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-DATABASE_URI = r"postgresql+psycopg2://postgres:MargretThacheris100%sexy@localhost:5432/books"
